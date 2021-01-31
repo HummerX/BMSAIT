1ee1899d086769fae738b21cdaeaa74a71df62ea
HummerX <59174922+HummerX@users.noreply.github.com>
2021-01-31 18:35:05 +0100

    Add files via upload

--- 04 Beispielprogramme/04 7-Segment-Anzeige mit TM1637/Arduino Sketch/BMSAIT_TM1637/BMSAIT_SSegTM1637.h
+++ 04 Beispielprogramme/04 7-Segment-Anzeige mit TM1637/Arduino Sketch/BMSAIT_TM1637/BMSAIT_SSegTM1637.h
@@ -1,48 +1,40 @@
 // settings and functions to display numbers on 7-segment-displays
+//target= device ID (if multiple displays are connected
+//ref2= not used
+//ref3= number of figures to display
+//ref4= offset position 
+//ref5= decimal point
 
 #include <TM1637Display.h>
 
 // Call 7-Segment-Display with TM1637 controller
 #define TM1637_CLK 2
 #define TM1637_DIO 3
+#define TM1637_BRIGHTNESS 0x0f
 
-//empty call
-TM1637Display TM1637_display=TM1637Display(0, 0);
+TM1637Display TM1637_display[] = {
+    TM1637Display(TM1637_CLK, TM1637_DIO)
+    // ,TM1637Display(/*CLK PIN*/,/* DIO PIN */)
+};
+
 
   
 void SetupTM1637()
 {
-  //initialize display
-  TM1637Display TM1637_display=TM1637Display(TM1637_CLK, TM1637_DIO);
-
-  //set brightness
-  TM1637_display.setBrightness(0x0f);
-
-  //clear display
-  TM1637_display.clear();
+  TM1637_display[0].setBrightness(TM1637_BRIGHTNESS);
+  TM1637_display[0].clear();
+  //TM1637_display[1].setBrightness(TM1637_BRIGHTNESS);
+  //TM1637_display[1].clear();
 }
 
-///display a number on the TM1637 display
+//display a 4-digit number from the flight data on the display
 void UpdateTM1637(int p)
 {
-    //read first n digits from the stored value and save this into a buffer
-    byte buf[datenfeld[p].stellen+1];
-    for (byte x;x<datenfeld[p].stellen;x++) 
-    {buf[x]=datenfeld[p].wert[x];}
-    buf[datenfeld[p].stellen]='\0';
-    //convert the buffer to a decimal number
-    uint16_t seg= atoi(buf);
-
-    //display the number
-    TM1637_display.showNumberDecEx(seg, 0b01000000,true,datenfeld[p].stellen,datenfeld[p].start);
-
-  /*
   uint16_t number = atoi(datenfeld[p].wert);
   uint8_t dots;
-  if (datenfeld[p].dp==0) dots=0b10000000;
-  if (datenfeld[p].dp==1) dots=0b01000000;
-  if (datenfeld[p].dp==2) dots=0b00100000;
-  if (datenfeld[p].dp==3) dots=0b00010000;
-  TM1637_display.showNumberDecEx(number, dots,true);
-  */
+  if (datenfeld[p].ref5==0) dots=0b10000000;
+  if (datenfeld[p].ref5==1) dots=0b01000000;
+  if (datenfeld[p].ref5==2) dots=0b00100000;
+  if (datenfeld[p].ref5==3) dots=0b00010000;
+  TM1637_display[datenfeld[p].target].showNumberDecEx(number, dots,true);
 }
