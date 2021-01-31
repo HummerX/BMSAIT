fb01f2da317b73b78670c186aac3a6af372ca4bf
HummerX <59174922+HummerX@users.noreply.github.com>
2021-01-31 18:35:32 +0100

    Add files via upload

--- 04 Beispielprogramme/05 Servo/Arduino Sketch/BMSAIT_Servo/BMSAIT_Servo.h
+++ 04 Beispielprogramme/05 Servo/Arduino Sketch/BMSAIT_Servo/BMSAIT_Servo.h
@@ -1,40 +1,38 @@
 // settings and functions to drive servo motors (directly connected to the arduino)
+//target= reference link to the line of the servodata table of this module
+//ref2= not used
+//ref3= not used
+//ref4= not used
+//ref5= not used
 
 #include <Servo.h>
 
+#define SERVODELAY 200
 
 typedef struct
 {
-  byte PIN;       //PIN the motor is connected to
-  byte p_ug;      //min arc for the motor (must not be less than 0!)
-  byte p_og;      //max arc for the motor (must not be more than 180!)
-  int a_ug;       //min absolut value that might be displayed (i.e.   0 for a speed indicator)
-  int a_og;       //max absolut value that might be displayed (i.e. 800 for a speed indicator)
-  int last;       //previous value
-  long lu;        //last update
-}Servodata;
+byte pIN;           //PIN the motor is connected to
+byte p_ug;          //min arc for the motor (must not be less than 0!)
+byte p_og;          //max arc for the motor (must not be more than 180!)
+int a_ug;           //min absolut value that might be displayed (i.e.   0 for a speed indicator)
+int a_og;           //max absolut value that might be displayed (i.e. 800 for a speed indicator)
+int last;           //previous value
+unsigned long lu;   //last update
+} Servodata;
 
-
-
-#define SERVODELAY 200
-
-//IMPORTANT:  You need to calibrate each servo to prevent damage.
-//during startup, each servo will be moved to the min/max value and stay there for 1 second. If the servo is vibrating in a min or max position, the position is out of bounds. 
-//in this case, you need to increase the min value (p_ug) or reduce the max value (p_og) in the following table until the servo will not vibrate in the min/max positions
 
 Servodata servodata[] =
-{
-// PIN  p_ug   p_og   a_ug   a_og   lu
-   {2,    10,   175,     0,   110,   0}  // example rpm indicator: {Servo on PIN=2, min arc =10, max arc=175, lowest possible data value=0% RPM, highest possible data value=110% RPM, past value=0}
+{// pIN  p_ug  p_og  a_ug  a_og  last  lu
+    {8,   0,   180,   0,   800,    0,   0},  // example speed indicator: {Servo on PIN=9, min arc =0, max arc=180, lowest possible value=0, highest value=800, past value=0, lastUpdate=0}
 };
 const int SERVOZAHL = sizeof(servodata)/sizeof(servodata[0]);
 Servo servo[SERVOZAHL];
 
-SetupServo()
+void SetupServo()
 {  
   for (byte lauf=0;lauf<SERVOZAHL;lauf++)
   {
-    servo[lauf].attach(servodata[lauf].PIN);
+    servo[lauf].attach(servodata[lauf].pIN);
     servo[lauf].write(servodata[lauf].p_ug);
     delay(1000);
     servo[lauf].write(servodata[lauf].p_og);
@@ -50,7 +48,7 @@ void Servo_Zeroize(void)
   for (byte lauf=0;lauf<SERVOZAHL;lauf++)
   {
     if (!servo[lauf].attached())
-        {servo[lauf].attach(servodata[lauf].PIN);}  //reactivate servo
+        {servo[lauf].attach(servodata[lauf].pIN);}  //reactivate servo
     servo[lauf].write(servodata[lauf].p_ug);
     delay(1000);
     servo[lauf].write(servodata[lauf].p_og);
@@ -61,28 +59,24 @@ void Servo_Zeroize(void)
   }
 }
 
+
 void UpdateServo(byte id)
 {
-  if (servodata[datenfeld[id].ziel].lu + 5000 < millis())
+  if (servodata[datenfeld[id].target].lu + 5000 < millis())
   {
-    servo[datenfeld[id].ziel].detach();                        //disable servo if no new signal arrived for more than 5 seconds
+    servo[datenfeld[id].target].detach();                        //disable servo if no new signal for more than 5 seconds
   }  
-  else if (servodata[datenfeld[id].ziel].lu + SERVODELAY < millis())  
+  else if (servodata[datenfeld[id].target].lu + SERVODELAY < millis())
   {
-    //check for new servo movement
-    servodata[datenfeld[id].ziel].lu = millis(); 
-    int val = atoi(datenfeld[id].wert);
+    servodata[datenfeld[id].target].lu = millis();  //pause servo
     
-    if (servodata[datenfeld[id].ziel].last != val)
+    if (servodata[datenfeld[id].target].last != atoi(datenfeld[id].wert))
     {
-      //initiate new movement
-      servodata[datenfeld[id].ziel].last = val; 
-      if (!servo[datenfeld[id].ziel].attached())
-        {servo[datenfeld[id].ziel].attach(servodata[datenfeld[id].ziel].PIN);}  //reactivate servo
-      //calculate the new arc position for the servo
-      uint16_t winkel = map(servodata[datenfeld[id].ziel].last, servodata[datenfeld[id].ziel].a_ug, servodata[datenfeld[id].ziel].a_og, servodata[datenfeld[id].ziel].p_ug, servodata[datenfeld[id].ziel].p_og); 
-      //move servo
-      servo[datenfeld[id].ziel].write(winkel);          
+      servodata[datenfeld[id].target].last = atoi(datenfeld[id].wert); 
+      if (!servo[datenfeld[id].target].attached())
+        {servo[datenfeld[id].target].attach(servodata[datenfeld[id].target].pIN);}  //reactivate servo
+      uint16_t winkel = map(servodata[datenfeld[id].target].last, servodata[datenfeld[id].target].a_ug, servodata[datenfeld[id].target].a_og, servodata[datenfeld[id].target].p_ug, servodata[datenfeld[id].target].p_og); 
+      servo[datenfeld[id].target].write(winkel);          
     }
   }
 }
