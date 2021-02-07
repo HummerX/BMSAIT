b445a3e52ddf7bfccd1a641cca3cfc145a4c21d9
HummerX <59174922+HummerX@users.noreply.github.com>
2021-02-07 11:38:36 +0100

    Add files via upload

--- 04 Beispielprogramme/04 7-Segment-Anzeige mit TM1637/Arduino Sketch/BMSAIT_TM1637/BMSAIT_SSegTM1637.h
+++ 04 Beispielprogramme/04 7-Segment-Anzeige mit TM1637/Arduino Sketch/BMSAIT_TM1637/BMSAIT_SSegTM1637.h
@@ -17,14 +17,26 @@
 #endif
 
 // Call 7-Segment-Display with TM1637 controller
-#define TM1637_SDA_PIN 2 //DIO
-#define TM1637_SCL_PIN 3 //CLK
-#define TM1637_BRIGHTNESS 0x0f
+#define TM1637_DIGITS 4 //number of digits on your TM1637 display (4 or 6)
+#define TM1637_SDA_PIN A4 //DIO
+#define TM1637_SCL_PIN A5 //CLK
+#define TM1637_BRIGHTNESS 10
 
-LEDDisplayDriver TM1637_display[] = {
-    LEDDisplayDriver(TM1637_SDA_PIN, TM1637_SCL_PIN,true, 6)
-    //LCDDisplayDriver(/*SDA_PIN*/,/* SCL_PIN*/,true, /*num of digits*/)
-};
+
+
+#if TM1637_DIGITS==6
+  LEDDisplayDriver TM1637_display[] = 
+  {
+      LEDDisplayDriver(TM1637_SDA_PIN, TM1637_SCL_PIN,true, TM1637_DIGITS)
+      //LCDDisplayDriver(/*SDA_PIN*/,/* SCL_PIN*/,true, /*num of digits*/)
+  };
+#else
+  LEDDisplayDriver TM1637_display[] = 
+  {
+      LEDDisplayDriver(TM1637_SDA_PIN, TM1637_SCL_PIN)
+      //LCDDisplayDriver(/*SDA_PIN*/,/* SCL_PIN*/)
+  };
+#endif
 
 
 void SetupTM1637()
@@ -38,10 +50,18 @@ void SetupTM1637()
 
 void UpdateTM1637(int p)
 {
+  byte disp[6] ={0,0,0,0,0,0};
+  memcpy(disp,datenfeld[p].wert,datenfeld[p].ref3);
+
+
+  
   byte pos=0;
-  byte decimal;
   for (byte x=0;x<datenfeld[p].ref3;x++)
   {
+    
+    #if TM1637_DIGITS==6
+    
+    //correct digit order
     switch (5-x-datenfeld[p].ref4)
     {
       case 0: 
@@ -63,9 +83,16 @@ void UpdateTM1637(int p)
         pos=2; 
         break;
     }
+    #else
+      //no correction required
+      pos=x;
+    #endif
+  
+
     if ((datenfeld[p].ref5-1)==x)
-      TM1637_display[datenfeld[p].target].showNumWithPoint(datenfeld[p].wert[x]-48,0,pos,1);
+      TM1637_display[datenfeld[p].target].showNumWithPoint(disp[x]-48,0,pos,1);
     else 
-      TM1637_display[datenfeld[p].target].showNum(datenfeld[p].wert[x]-48,pos,1);
+      TM1637_display[datenfeld[p].target].showNum(disp[x]-48,pos,1);
+
   }
 }
