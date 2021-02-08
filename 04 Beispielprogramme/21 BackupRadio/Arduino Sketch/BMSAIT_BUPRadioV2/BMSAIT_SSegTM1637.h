5bed7333e1935602d7bb1c59760940ed3bdded9b
HummerX <59174922+HummerX@users.noreply.github.com>
2021-02-08 23:36:33 +0100

    Add files via upload

--- 04 Beispielprogramme/21 BackupRadio/Arduino Sketch/BMSAIT_BUPRadioV2/BMSAIT_SSegTM1637.h
+++ 04 Beispielprogramme/21 BackupRadio/Arduino Sketch/BMSAIT_BUPRadioV2/BMSAIT_SSegTM1637.h
@@ -17,14 +17,24 @@
 #endif
 
 // Call 7-Segment-Display with TM1637 controller
-#define TM1637_SDA_PIN 2 //DIO
-#define TM1637_SCL_PIN 3 //CLK
+#define TM1637_DIGITS 4 //number of digits on your TM1637 display (4 or 6)
+#define TM1637_SDA_PIN A4 //DIO
+#define TM1637_SCL_PIN A5 //CLK
 #define TM1637_BRIGHTNESS 0x0f
 
-LEDDisplayDriver TM1637_display[] = {
-    LEDDisplayDriver(TM1637_SDA_PIN, TM1637_SCL_PIN,true, 6)
-    //LCDDisplayDriver(/*SDA_PIN*/,/* SCL_PIN*/,true, /*num of digits*/)
-};
+#if TM1637_DIGITS==6
+  LEDDisplayDriver TM1637_display[] = 
+  {
+      LEDDisplayDriver(TM1637_SDA_PIN, TM1637_SCL_PIN,true, TM1637_DIGITS)
+      //LCDDisplayDriver(/*SDA_PIN*/,/* SCL_PIN*/,true, /*num of digits*/)
+  };
+#else
+  LEDDisplayDriver TM1637_display[] = 
+  {
+      LEDDisplayDriver(TM1637_SDA_PIN, TM1637_SCL_PIN)
+      //LCDDisplayDriver(/*SDA_PIN*/,/* SCL_PIN*/)
+  };
+#endif
 
 
 void SetupTM1637()
@@ -43,32 +53,39 @@ void UpdateTM1637(int p)
     //memcpy(disp,datenfeld[p].wert,datenfeld[p].ref3);
     memcpy(disp,Wert,datenfeld[p].ref3);
   //mod
+  
   byte pos=0;
-  byte decimal;
   for (byte x=0;x<datenfeld[p].ref3;x++)
   {
-    switch (5-x-datenfeld[p].ref4)
-    {
-      case 0: 
-        pos=3; 
-        break;
-      case 1: 
-        pos=4; 
-        break;
-      case 2: 
-        pos=5; 
-        break;
-      case 3: 
-        pos=0; 
-        break;
-      case 4: 
-        pos=1; 
-        break;
-      case 5: 
-        pos=2; 
-        break;
-    }
-    if ((datenfeld[p].ref5-1)==x)
+    #if TM1637_DIGITS==6
+      //correct digit order
+      switch (5-x-datenfeld[p].ref4)
+      {
+        case 0: 
+          pos=3; 
+          break;
+        case 1: 
+          pos=4; 
+          break;
+        case 2: 
+          pos=5; 
+          break;
+        case 3: 
+          pos=0; 
+          break;
+        case 4: 
+          pos=1; 
+          break;
+        case 5: 
+          pos=2; 
+          break;
+      }
+    #else
+      //no correction required
+      pos=x;
+    #endif
+  
+    if ((datenfeld[p].ref5-1==x)&&(disp[x]!=' '))
       TM1637_display[datenfeld[p].target].showNumWithPoint(disp[x]-48,0,pos,1);
     else 
       TM1637_display[datenfeld[p].target].showNum(disp[x]-48,pos,1);
