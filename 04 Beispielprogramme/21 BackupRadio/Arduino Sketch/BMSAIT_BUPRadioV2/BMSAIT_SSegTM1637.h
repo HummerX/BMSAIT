7819aa7db6c1b097216e1175c05d0f090f43e525
HummerX <59174922+HummerX@users.noreply.github.com>
2023-03-07 20:13:38 +0100

    Add files via upload

--- 04 Beispielprogramme/21 BackupRadio/Arduino Sketch/BMSAIT_BUPRadioV2/BMSAIT_SSegTM1637.h
+++ 04 Beispielprogramme/21 BackupRadio/Arduino Sketch/BMSAIT_BUPRadioV2/BMSAIT_SSegTM1637.h
@@ -17,22 +17,27 @@
 #endif
 
 // Call 7-Segment-Display with TM1637 controller
-#define TM1637_DIGITS 4 //number of digits on your TM1637 display (4 or 6)
-#define TM1637_SDA_PIN A4 //DIO
-#define TM1637_SCL_PIN A5 //CLK
-#define TM1637_BRIGHTNESS 0x0f
+#define TM1637_DIGITS 6 //number of digits on your TM1637 display (4 or 6)
+#define TM1637_SDA1_PIN  13  //DIO
+#define TM1637_SCL1_PIN  12  //CLK
+//#define TM1637_SDA2_PIN  xx  //DIO
+//#define TM1637_SCL2_PIN  xx  //CLK
+
+#define TM1637_BRIGHTNESS 10
+
+
 
 #if TM1637_DIGITS==6
   LEDDisplayDriver TM1637_display[] = 
   {
-      LEDDisplayDriver(TM1637_SDA_PIN, TM1637_SCL_PIN,true, TM1637_DIGITS)
-      //LCDDisplayDriver(/*SDA_PIN*/,/* SCL_PIN*/,true, /*num of digits*/)
+      LEDDisplayDriver(TM1637_SDA1_PIN, TM1637_SCL1_PIN,true, TM1637_DIGITS)
+      //,LCDDisplayDriver(TM1637_SDA2_PIN, TM1637_SCL2_PIN,true, TM1637_DIGITS)
   };
 #else
   LEDDisplayDriver TM1637_display[] = 
   {
-      LEDDisplayDriver(TM1637_SDA_PIN, TM1637_SCL_PIN)
-      //LCDDisplayDriver(/*SDA_PIN*/,/* SCL_PIN*/)
+      LEDDisplayDriver(TM1637_SDA1_PIN, TM16371_SCL_PIN)
+      //,LCDDisplayDriver(TM1637_SDA2_PIN, TM1637_SCL2_PIN)
   };
 #endif
 
@@ -45,49 +50,65 @@ void SetupTM1637()
   //TM1637_display[1].clear();
 }
 
+byte TM1637_TransformPos(byte digit)
+{
+  switch (digit)
+  {
+    case 0: 
+      return 3; 
+    case 1: 
+      return 4; 
+    case 2: 
+      return 5; 
+    case 3: 
+      return 0; 
+    case 4: 
+      return 1; 
+    case 5: 
+      return 2; 
+  }  
+}
 
 void UpdateTM1637(int p)
 {
-  byte disp[6] ={0,0,0,0,0,0};
-  //mod
-    //memcpy(disp,datenfeld[p].wert,datenfeld[p].ref3);
-    memcpy(disp,Wert,datenfeld[p].ref3);
-  //mod
-  
+  #if TM1637_DIGITS==6
+    byte disp[6] ={0,0,0,0,0,0};
+  #else
+    byte disp[4] ={0,0,0,0};
+  #endif
+  memcpy(Wert,datenfeld[p].wert,datenfeld[p].ref3);
+  BUPRadioUpdate(p);
+  memcpy(disp,Wert,datenfeld[p].ref3);
   byte pos=0;
+
+  //write digits
   for (byte x=0;x<datenfeld[p].ref3;x++)
   {
-    #if TM1637_DIGITS==6
+    #if TM1637_DIGITS == 6
       //correct digit order
-      switch (5-x-datenfeld[p].ref4)
-      {
-        case 0: 
-          pos=3; 
-          break;
-        case 1: 
-          pos=4; 
-          break;
-        case 2: 
-          pos=5; 
-          break;
-        case 3: 
-          pos=0; 
-          break;
-        case 4: 
-          pos=1; 
-          break;
-        case 5: 
-          pos=2; 
-          break;
-      }
+      pos=TM1637_TransformPos(5-x-datenfeld[p].ref4);
     #else
       //no correction required
       pos=x;
     #endif
+        
+    TM1637_display[datenfeld[p].target].showChar(pos,disp[x]);
+  }
   
-    if ((datenfeld[p].ref5-1==x)&&(disp[x]!=' '))
-      TM1637_display[datenfeld[p].target].showNumWithPoint(disp[x]-48,0,pos,1);
-    else 
-      TM1637_display[datenfeld[p].target].showNum(disp[x]-48,pos,1);
+  //paint decimal point
+  if(datenfeld[p].ref5<=datenfeld[p].ref3)  
+  {
+    if (disp[datenfeld[p].ref5]==' ')
+      { TM1637_display[datenfeld[p].target].removeDp();}
+    else
+    {  
+      #if TM1637_DIGITS == 6
+        //correct digit order
+        TM1637_display[datenfeld[p].target].setDp(TM1637_TransformPos(5-datenfeld[p].ref5-datenfeld[p].ref4+1));
+      #else
+        //no correction required
+        TM1637_display[datenfeld[p].target].setDp(5-datenfeld[p].ref5-datenfeld[p].ref4+1);
+      #endif
+    }
   }
 }
