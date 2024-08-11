5b371a35b34978d3297e9283f4abed86eea6b595
HummerX <59174922+HummerX@users.noreply.github.com>
2024-08-12 00:40:42 +0200

    Add files via upload
    Code correction due to reported issue.

--- 04 Beispielprogramme/11 Buttonmatrix/Arduino Sketch/BMSAIT_DemoMatrix/BMSAIT_Buttonmatrix.h
+++ 04 Beispielprogramme/11 Buttonmatrix/Arduino Sketch/BMSAIT_DemoMatrix/BMSAIT_Buttonmatrix.h
@@ -10,64 +10,92 @@ const int colCount = sizeof(cols)/sizeof(cols[0]);
 byte keys[colCount][rowCount];
 
 //define the signal to be send to the BMSAIT App when a button is pressed
-byte keysignal[colCount][rowCount]=
-
+char keyDown[colCount][rowCount][3]=
 {
-// row1  row2   row3 
- {   1,    2,    3 } //col 1
-,{   4,    5,    6 } //col 2
+// row1       row2       row3 
+ {   "01",    "02",    "03" } //col 1
+,{   "04",    "06",    "00" } //col 2
 };
+
+//define the signal to be send to the BMSAIT App when a button is released
+char keyUp[colCount][rowCount][3]=
+{
+// row1       row2       row3 
+ {   "00",    "00",    "00" } //col 1
+,{   "05",    "07",    "00" } //col 2
+};
+
+//memorize last switch position
+byte keyStatus[colCount][rowCount];   
+
 
 void SetupButtonMatrix() 
 {
     for(int rowIndex=0; rowIndex<rowCount; rowIndex++)
     {
-        pinMode(rows[rowIndex], INPUT);
+        pinMode( rows[rowIndex], INPUT_PULLUP);
     }
     for (int colIndex=0; colIndex<colCount; colIndex++) 
     {
-        pinMode(cols[colIndex], INPUT_PULLUP);
+        pinMode(cols[colIndex], OUTPUT);
+        digitalWrite(cols[colIndex], HIGH);
     }    
-}
- 
-void ButtonmatrixRead() 
-{
-    for (int colIndex=0; colIndex < colCount; colIndex++) 
+
+    for (int colIndex=0; colIndex<colCount; colIndex++) 
     {
-        // interate through the columns
-        byte curCol = cols[colIndex];
-        pinMode(curCol, OUTPUT);
-        digitalWrite(curCol, LOW);
- 
-        // interate through the rows
-        for (int rowIndex=0; rowIndex < rowCount; rowIndex++) 
-        {
-            byte rowCol = rows[rowIndex];
-            pinMode(rowCol, INPUT_PULLUP);
-            if (digitalRead(rowCol)==0)
-            {
-              if (keys[colIndex][rowIndex]==0)
-              {
-                delay(10); //wait to make sure that this is real button press
-                if (keys[colIndex][rowIndex]==0)
-                {
-                  char buf[4]; 
-                  itoa(keysignal[colIndex][rowIndex],buf,10);
-                  buf[3]='\0';
-                  SendMessage(buf,3);
-                  keys[colIndex][rowIndex] = 1;
-                }
-              }  
-            }
-            else
-            {
-              keys[colIndex][rowIndex] = 0;
-            }
-            //disable the row
-            pinMode(rowCol, INPUT);
-        }
-        // disable the column
-        pinMode(curCol, INPUT);
+      for(int rowIndex=0; rowIndex<rowCount; rowIndex++)
+      {
+         keyStatus[colIndex][rowIndex]=0;
+      }
     }
+}
+
+ 
+void ButtonMatrixRead() 
+{  
+  for (int colIndex=0; colIndex < colCount; colIndex++) 
+  {
+      // iterate through the columns
+      byte curCol = cols[colIndex];
+      
+      digitalWrite(curCol, LOW);
+
+      // iterate through the rows
+      for (int rowIndex=0; rowIndex < rowCount; rowIndex++) 
+      { 
+          byte curRow = rows[rowIndex];
+          if (digitalRead(curRow)!=keyStatus[colIndex][rowIndex])
+          {
+            delay(5); //debouncing
+            if (digitalRead(curRow)!=keyStatus[colIndex][rowIndex])
+            {
+              if (keyStatus[colIndex][rowIndex]==1)
+              {
+                //key pressed
+                keyStatus[colIndex][rowIndex]=0;
+                if ((keyDown[colIndex][rowIndex][0] != '0') || (keyDown[colIndex][rowIndex][1] != '0')) // only send a message if a command is defined
+                {
+                  SendMessage(keyDown[colIndex][rowIndex],3);
+                  delay(50);
+                }
+              }
+              else
+              {
+                //key released
+                keyStatus[colIndex][rowIndex]=1;
+                if ((keyUp[colIndex][rowIndex][0] != '0') || (keyUp[colIndex][rowIndex][1] != '0')) // only send a message if a command is defined
+                {
+                  SendMessage(keyUp[colIndex][rowIndex],3);
+                  delay(50);
+                }
+              }
+            }  
+          }
+          //disable the row
+
+      }
+      // disable the column
+      digitalWrite(curCol, HIGH);
+  }
 }
  
