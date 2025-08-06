28af84198010f396cf553b99f932646df8378b87
Monstertov <rob@monstertov.nl>
2025-08-06 22:17:18 +0200

    Improved clarity by fixing typo: 'recieved' corrected to 'received'

--- 04 examples/53 Trim Panel/Arduino Sketch/BMSAIT_DemoTrimPanel/BMSAIT_DemoTrimPanel.ino
+++ 04 examples/53 Trim Panel/Arduino Sketch/BMSAIT_DemoTrimPanel/BMSAIT_DemoTrimPanel.ino
@@ -953,7 +953,7 @@ void ReadResponse()
   }
 }
 
-//readback of recieved data for verification
+//readback of received data for verification
 void DebugReadback(byte posID)
 {
   char antwort[DATENLAENGE+3]="";
